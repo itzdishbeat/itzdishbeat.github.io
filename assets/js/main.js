2327bac69e1b0bb9e3d014d452e373791729e014
itzdishbeat <106402059+itzdishbeat@users.noreply.github.com>
2023-12-29 11:34:51 +0300

    Добавление карточки товара, багфиксы, обновление правил табуляции в CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,41 @@ categoryBtns.forEach((category, index) => {
         categoryContent[index].classList.add("active")
     })
 })
+
+// About Product
+
+const aboutFullBtn = document.querySelector(".about-full-btn");
+const aboutFullInfo = document.querySelector(".about-more");
+const aboutFullBtnText = document.querySelector(".about-full-btn-text");
+const aboutFullBtnIcon = document.querySelector(".about-full-btn svg");
+aboutFullBtn.addEventListener("click", () => {
+    var x = document.getElementById("aboutMore");
+    if (x.className === "about-more") {
+        x.className += " active";
+        window.location = "#aboutMore";
+        aboutFullBtnText.innerHTML = "Меньше";
+        aboutFullBtnIcon.style.transform = 'rotate(-180deg)';
+
+    } else {
+        x.className = "about-more";
+        aboutFullBtnText.innerHTML = "Смотреть все";
+        aboutFullBtnIcon.style.transform = 'rotate(0deg)';
+    }
+});
+
+// Accordion
+
+var accordions = document.getElementsByClassName("accordion");
+for (var i = 0; i < accordions.length; i++) {
+    accordions[i].onclick = function() {
+    this.classList.toggle('is-open');
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+    // accordion is currently open, so close it
+        content.style.maxHeight = null;
+    } else {
+    // accordion is currently closed, so open it
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+}
+}
